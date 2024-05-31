9741ea87ceaa031d68142700db0bcf4a8a340e13
lenafam96 <lecongminh@MacBook-Pro-cua-Le.local>
2024-05-31 11:06:24 +0700

    update vps

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ import json
 import base64
 import cv2
 import numpy as np
-# from tool import solve_sudoku
+from tool import solve_sudoku
 
 app = Flask(__name__)
 
@@ -17,21 +17,21 @@ cors = CORS(app, resources={r"/*": {"origins": "https://sudoku.com"}})
 def index():
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
-# @app.route('/upload', methods=['POST'])
-# def board():
-#     data = request.get_json()
-#     imgstring = data.get('image')
-#     imageData = base64.b64decode(imgstring)
-#     nparr = np.frombuffer(imageData, np.uint8)
-#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+@app.route('/upload', methods=['POST'])
+def board():
+    data = request.get_json()
+    imgstring = data.get('image')
+    imageData = base64.b64decode(imgstring)
+    nparr = np.frombuffer(imageData, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-#     # add border 100px white color to the image
-#     img = cv2.copyMakeBorder(img, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+    # add border 100px white color to the image
+    img = cv2.copyMakeBorder(img, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=[255, 255, 255])
 
-#     board, origin = solve_sudoku(img)
-#     print(board)
+    board, origin = solve_sudoku(img)
+    print(board)
     
-#     return json.dumps({'success':True, 'data': {'origin': origin, 'board': board}}), 200, {'ContentType':'application/json'}
+    return json.dumps({'success':True, 'data': {'origin': origin, 'board': board}}), 200, {'ContentType':'application/json'}
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -40,6 +40,6 @@ def page_not_found(e):
 
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=1026)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1026)
     # app.run(debug=True)
